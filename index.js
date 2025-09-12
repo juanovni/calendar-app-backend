98cc32e83371bfbb1098628f2dc49c9d73e4ff3a
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-12 11:18:51 -0500

    Created the express port and method

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hola Mundo");
+const express = require("express");
+
+
+// CREAR SERVIDOR EXPRESS
+const app = express();
+
+// PETICIONES
+app.get("/", (req, resp) => {
+    resp.json({
+        ok: true,
+    });
+})
+
+
+// ESCUCHA PETICIONES
+app.listen("4000", () => {
+    console.log("Servidor correriendo en el puerto 4000")
+})
