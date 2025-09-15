89e9dab0184061ec815c059f8524cb9b3e7aa63f
vista-juan-constantine <juan.constantine@vistadash.com>
2025-09-15 12:58:02 -0500

    Added the events schema

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require('express'); // No vuelve a hacer la carga porque esta en memoria
+const Event = require("../models/Event");
 
 const getEvents = async (req, res = response) => {
     res.status(201).json({
@@ -8,10 +9,27 @@ const getEvents = async (req, res = response) => {
 }
 
 const addEvents = async (req, res = response) => {
-    res.status(201).json({
-        ok: true,
-        msg: 'Add Events',
-    })
+
+
+    try {
+        const eventDB = new Event(req.body)
+        eventDB.user = req.uid;
+
+        await eventDB.save();
+
+
+        res.status(201).json({
+            ok: true,
+            msg: 'Add Events',
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            ok: false,
+            msg: "Error al crear el user",
+        })
+    }
+
 }
 
 const updateEvents = async (req, res = response) => {
--- routes/events.js
+++ routes/events.js
@@ -6,6 +6,9 @@
 const { Router } = require("express");
 const { validateJWT } = require("../middlewares/validate-jwt")
 const { getEvents, addEvents, updateEvents, deleteEvents } = require("../controllers/events")
+const { check } = require('express-validator');
+const { validateFields } = require("../middlewares/validate-fields");
+const { isDate } = require("../helpers/isDate");
 
 const route = Router();
 
@@ -19,7 +22,14 @@ route.post("/", addEvents); */
 
 // 2. Forma
 route.get("/", getEvents);
-route.post("/", addEvents);
+
+route.post("/", [
+    check("title", "El titulo es obligatorio").not().isEmpty(),
+    check("start", "Fecha de inicio es obligatorio").custom(isDate),
+    check("end", "Fecha fin es obligatorio").custom(isDate),
+    validateFields
+], addEvents);
+
 route.put("/:id", updateEvents);
 route.delete("/:id", deleteEvents);
 
